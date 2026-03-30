eaae4dab6d8a21d64d2982fc4e02cbc8a41fb58b
areebakhund25 <areebakhund25@gmail.com>
2026-03-31 00:29:47 +0500

    Change server port from 5000 to 5001

--- app.py
+++ app.py
@@ -116,4 +116,4 @@ def page_not_found(e):
 if __name__ == '__main__':
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
     print("ShopWise.com is running at http://127.0.0.1:5000")
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5001)
